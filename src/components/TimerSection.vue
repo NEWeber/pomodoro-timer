fd67e0180314ed7adf588b6180d233c43d4ebacc
NEWeber <nicholaseweber@gmail.com>
2018-08-31 19:37:54 -0700

    Start basics of timer section

---
+++ src/components/TimerSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="time-display">
+      {{ timeLeft }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimerSection',
+};
+
+</script>
+
+<style lang="sass">
+
+</style>
