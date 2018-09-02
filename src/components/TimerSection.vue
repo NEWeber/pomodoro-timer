420dad64cf9b03c6c46182c64138e38a577c794f
NEWeber <nicholaseweber@gmail.com>
2018-09-01 20:08:48 -0700

    Add countdown component that works on button click

--- src/components/TimerSection.vue
+++ src/components/TimerSection.vue
@@ -1,15 +1,60 @@
 <template>
   <div>
     <div class="time-display">
-      <!-- {{ timeLeft }} -->
-      25:00
+      {{minutesLeft}}:{{secondsLeft}} <br/>
     </div>
+    <button @click="startTimer()">Start Timer!</button>
   </div>
 </template>
 
 <script>
+import { debounce } from 'lodash';
+
 export default {
   name: 'TimerSection',
+  data() {
+    return {
+      startTime: 0,
+      timerRunning: false,
+      startingMinutes: 25,
+      startingSeconds: 60,
+      minutesLeft: 25,
+      secondsLeft: 0,
+    };
+  },
+  watch: {
+    timerRunning() {
+      if (this.timerRunning) {
+        this.debouncedUpdateTime();
+      }
+    },
+  },
+  created() {
+    this.debouncedUpdateTime = debounce(this.updateTime, 500);
+  },
+  methods: {
+    updateTime() {
+      const currentTime = Date.now();
+      const totalSecondsElapsed = Math.floor((currentTime - this.startTime) / 1000);
+      const secondsElapsed = totalSecondsElapsed % 60;
+      // So that the time rolls over properly
+      // (i.e. immediatly goes to 24 on start, then 24:01, 24:00, 23:59)
+      const extraMinute = secondsElapsed > 0 ? 1 : 0;
+      const minutesElapsed = ((totalSecondsElapsed - secondsElapsed) / 60) + extraMinute;
+      this.minutesLeft = this.startingMinutes - minutesElapsed;
+      if (secondsElapsed === 0) {
+        this.secondsLeft = 0;
+      } else {
+        this.secondsLeft = this.startingSeconds - secondsElapsed;
+      }
+      if (this.secondsLeft <= 0 && this.minutuesLeft <= 0) this.timerRunning = false;
+      if (this.timerRunning) this.debouncedUpdateTime();
+    },
+    startTimer() {
+      this.startTime = Date.now();
+      this.timerRunning = true;
+    },
+  },
 };
 
 </script>
