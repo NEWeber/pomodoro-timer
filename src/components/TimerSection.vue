9f9a9587137266dfc24994b8aac0ceec1b6e99d3
NEWeber <nicholaseweber@gmail.com>
2018-09-01 20:08:48 -0700

    WIP on getting timer countdown to work

--- src/components/TimerSection.vue
+++ src/components/TimerSection.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <div class="time-display">
-      <!-- {{ timeLeft }} -->
+      <!-- {{ startTime -  }} -->
       25:00
     </div>
+    <button @click="startTimer()">Start Timer!</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TimerSection',
+  methods: {
+    startTimer() {
+      const startTime = new Date(Date.now());
+      console.log(startTime);
+      console.log(startTime.toTimeString(startTime));
+    },
+  },
+  // 25 - (now - startTime )
 };
 
 </script>
