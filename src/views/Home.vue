35ca9d6de052028b04b3719a7db60f9369ec9276
NEWeber <nicholaseweber@gmail.com>
2018-09-29 20:29:09 -0700

    Remove hello world page from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <TimerSection></TimerSection>
     <AudioSection></AudioSection>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-
   </div>
 </template>
 
